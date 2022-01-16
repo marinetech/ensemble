305ead964842bc20c95ed5e2567e5c86c4a401e9
ochebotar <kip0d@pm.me>
2022-01-16 15:30:40 +0200

    sync bouys

--- bouy_sync.sh
+++ bouy_sync.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-#sudo date --set="$(sshpass -p anliolr ssh pi@192.168.0.6 'date -u')"
 sshpass -p anliolr ssh pi@192.168.0.6 sudo date -s @$(sudo date -u +"%s")
 sshpass -p anliolr ssh pi@192.168.0.7 sudo date -s @$(sudo date -u +"%s")
 sshpass -p anliolr ssh pi@192.168.0.8 sudo date -s @$(sudo date -u +"%s")
+sshpass -p anliolr ssh pi@192.168.0.6 sh /home/pi/ensemble/ping_timing.sh &
+sshpass -p anliolr ssh pi@192.168.0.8 sh /home/pi/ensemble/ping_timing.sh &
+sshpass -p anliolr ssh pi@192.168.0.7 sh /home/pi/ensemble/ping_timing.sh &
+# sh /home/pi/ensemble/ping_timing.sh &
