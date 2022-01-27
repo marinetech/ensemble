dac7e41f9d3959399d699bb17c914c14f3456c52
ochebotar <kip0d@pm.me>
2022-01-27 14:59:44 +0200

    small changes

--- bouy_sync.sh
+++ bouy_sync.sh
@@ -3,6 +3,5 @@ sshpass -p anliolr ssh pi@192.168.0.6 sudo date -s @$(sudo date -u +"%s")
 sshpass -p anliolr ssh pi@192.168.0.7 sudo date -s @$(sudo date -u +"%s")
 sshpass -p anliolr ssh pi@192.168.0.8 sudo date -s @$(sudo date -u +"%s")
 sshpass -p anliolr ssh pi@192.168.0.6 sh /home/pi/ensemble/ping_timing.sh &
-sshpass -p anliolr ssh pi@192.168.0.8 sh /home/pi/ensemble/ping_timing.sh &
 sshpass -p anliolr ssh pi@192.168.0.7 sh /home/pi/ensemble/ping_timing.sh &
-# sh /home/pi/ensemble/ping_timing.sh &
+sshpass -p anliolr ssh pi@192.168.0.8 sh /home/pi/ensemble/ping_timing.sh &
